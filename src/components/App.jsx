90c977df04985eb9da63144619eeef05012c46a0
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-08-21 13:41:54 +0300

    change function increment, add prevState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,13 +29,13 @@ function App() {
     // }));
     switch (name) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
       default:
         return;
